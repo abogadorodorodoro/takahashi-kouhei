bb2e8cdcc27d662c268d9429594115ba4e8f07c7
abogadorodorodoro <t246112@st.yamagata-u.ac.jp>
2025-10-31 18:48:38 +0900

    Add files via upload

---
+++ mudamudamudamuda.c
@@ -0,0 +1,16 @@
+int main(void)
+{
+    int k;
+    printf("Do you remember how many slices of bread you've eaten so far?");
+    scanf("%d", &k);
+    if (k >= 1000)
+    {
+        printf("OMG");
+    }
+    else
+    {
+        printf("YOU SHALL NOT PASS!!");
+    }
+    
+    return 0;
+}
